9f1506b37ab551bb62fbc5db5e1b47e171dfa865
Literally Collective <bluedragon.cstar@outlook.com>
2021-06-29 17:06:03 -0300

    Fix: sidebar height when scrolling

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -25,7 +25,7 @@ const StyledPanel = styled.div<{ isPushed: boolean; showMenu: boolean }>`
   flex-shrink: 0;
   background-color: ${({ theme }) => theme.nav.background};
   width: ${({ isPushed }) => (isPushed ? `${SIDEBAR_WIDTH_FULL}px` : 0)};
-  height: calc(100vh - ${MENU_HEIGHT}px);
+  height: ${({ showMenu }) => (showMenu ? `calc(100vh - ${MENU_HEIGHT}px)` : `100vh`)};
   transition: padding-top 0.2s, width 0.2s;
   border-right: ${({ isPushed }) => (isPushed ? "2px solid rgba(133, 133, 133, 0.1)" : 0)};
   z-index: 11;
